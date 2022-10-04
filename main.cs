853b55c4713b6c957c6824c5a39f688ae0a6f8c9
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-04 18:46:59 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+// Created by:sophie
+// Created on: oct 2020
+//
+//this program displays, "hello world"
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("hey there world!");
+    Console.WriteLine ("\nDone.");
+  }
+}
