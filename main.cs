f6eede5c393bf521d8a3d5f7a832cb77ff2906de
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-05 17:49:18 +0000

    csharp

--- main.cs
+++ main.cs
@@ -7,7 +7,6 @@ using System;
 
 class Program {
     public static void Main (string[] args) {
-        Console.WriteLine ("Hello World");
         Console.WriteLine ("hey there world!");
         Console.WriteLine ("\nDone.");
     }
